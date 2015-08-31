9198a3384f5ee7e0c0a590d83c98c9283b3fefc8
tomagoes <tomagoes@users.noreply.github.com>
2015-08-31 13:34:16 -0500

    Update ecobeeEcho.groovy

--- ecobeeEcho.groovy
+++ ecobeeEcho.groovy
@@ -13,7 +13,6 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- * Change the mode manually (by pressing the app's play button) and automatically at the ecobee thermostat(s)
  */
 definition(
 	name: "ecobeeEcho",
